d4dc7eb6c5f53c658f4805e3f928f2e42ba397a7
roman hrytsiuk <rmngrck97@gmail.com>
2023-04-28 13:48:45 +0300

    configured showing tip animation every 10sec

--- src/app/reading/lesson3/lesson3.component.ts
+++ src/app/reading/lesson3/lesson3.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-lesson3',
   templateUrl: './lesson3.component.html',
   styleUrls: ['./lesson3.component.scss']
 })
-export class Lesson3Component {
+export class Lesson3Component implements OnInit, AfterViewInit, OnDestroy {
   lettersRemaining: number = 11
   progressValue: number = 0
+
+  private timer: any;
+
+  @ViewChild ('wordContainer') wordContainer: any
 
   words: string[] = [
     'абетка',
@@ -25,9 +29,44 @@ export class Lesson3Component {
     ['а', 'п', 'т', 'е', 'к', 'а']
   ]
 
+  ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit(): void {
+    this.startTimer()
+  }
+
+  startTimer() {
+    this.timer = setTimeout(() => {
+      this.showTip()
+      this.startTimer()
+    }, 10000);
+  }
+
+  resetTimer() {
+    clearTimeout(this.timer);
+    this.startTimer()
+  }
+
+  showTip() {
+    console.log('tip showed')
+    const children = this.wordContainer.nativeElement.children
+    for (let i = 0; i < children.length; i++) {
+      const letters = children[i].children
+      for (let j = 0; j < letters.length; j++) {
+        if (letters[j].textContent === 'а' && !letters[j].classList.contains('clicked')) {
+          letters[j].classList.add('tip')
+          setTimeout(() => {letters[j].classList.remove('tip')}, 1000)
+        }
+      }
+    }
+  }
+
   clickLetter(event: any) {
     const divideValue: number = 100 / 11
     if (!event.target.classList.contains('clicked') && event.target.textContent === 'а') {
+      this.resetTimer()
       event.target.classList.add('clicked')
       this.lettersRemaining--
       this.progressValue += divideValue
@@ -36,5 +75,9 @@ export class Lesson3Component {
       event.target.classList.add('wrong-letter')
       setTimeout(() => {event.target.classList.remove('wrong-letter')}, 1000)
     }
+  }
+
+  ngOnDestroy(): void {
+    clearTimeout(this.timer);
   }
 }
